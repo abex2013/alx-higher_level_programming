2b14aa4a4d6a28a2f55294e0a21bc3b56f45bf34
Abebayehu Tefera <abebayehutefera@gmail.com>
2023-07-04 13:02:09 -0700

    101-post_json.sh fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL with a given JSON file
-curl -s -H "Content-Type: appliation/json" -d "$(cat "$2")" "$1"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
